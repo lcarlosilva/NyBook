d75abb20e5b62122f69b1797cb4810972828e8da
Luiz Carlos <luiz.silva@luizalabs.com>
2020-06-13 01:30:32 -0300

    criacao da lista de Books

---
+++ app/src/main/java/com/example/nybooks/presetation/books/BooksActivity.kt
@@ -0,0 +1,34 @@
+package com.example.nybooks.presetation.books
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.nybooks.R
+import com.example.nybooks.data.model.Book
+import kotlinx.android.synthetic.main.activity_books.*
+
+class BooksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+
+        toolbarMain.title = getString(R.string.title_books)
+        setSupportActionBar(toolbarMain)
+
+        with(recyclerBooks) {
+            layoutManager = LinearLayoutManager(this@BooksActivity, RecyclerView.VERTICAL, false)
+            setHasFixedSize(true)
+            adapter = BooksAdapter(getBooks())
+        }
+    }
+
+    fun getBooks() : List<Book> {
+        return listOf<Book>(
+            Book("Titulo Um", "Autor A"),
+            Book("Titulo Dois", "Autor B"),
+            Book("Titulo Três", "Autor C")
+        )
+    }
+}
