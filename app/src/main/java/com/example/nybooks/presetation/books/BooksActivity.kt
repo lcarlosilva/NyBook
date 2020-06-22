d5eb6d1641a208f52a36cbcccaacb90187f7b97a
Luiz Carlos <luiz.silva@luizalabs.com>
2020-06-21 21:29:21 -0300

    constucao do consumo da API de 'lists.json'

--- app/src/main/java/com/example/nybooks/presetation/books/BooksActivity.kt
+++ app/src/main/java/com/example/nybooks/presetation/books/BooksActivity.kt
@@ -1,11 +1,12 @@
 package com.example.nybooks.presetation.books
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nybooks.R
-import com.example.nybooks.data.model.Book
 import kotlinx.android.synthetic.main.activity_books.*
 
 class BooksActivity : AppCompatActivity() {
@@ -17,18 +18,16 @@ class BooksActivity : AppCompatActivity() {
         toolbarMain.title = getString(R.string.title_books)
         setSupportActionBar(toolbarMain)
 
-        with(recyclerBooks) {
-            layoutManager = LinearLayoutManager(this@BooksActivity, RecyclerView.VERTICAL, false)
-            setHasFixedSize(true)
-            adapter = BooksAdapter(getBooks())
-        }
-    }
-
-    fun getBooks() : List<Book> {
-        return listOf<Book>(
-            Book("Titulo Um", "Autor A"),
-            Book("Titulo Dois", "Autor B"),
-            Book("Titulo Três", "Autor C")
-        )
+        val viewModel: BooksViewModel = ViewModelProviders.of(this).get(BooksViewModel::class.java)
+        viewModel.booksLiveData.observe(this, Observer {
+            it?.let { books ->
+                with(recyclerBooks) {
+                    layoutManager = LinearLayoutManager(this@BooksActivity, RecyclerView.VERTICAL, false)
+                    setHasFixedSize(true)
+                    adapter = BooksAdapter(books)
+                }
+            }
+        })
+        viewModel.getBooks()
     }
 }
--- app/src/main/java/com/example/nybooks/presetation/books/BooksViewModel.kt
+++ app/src/main/java/com/example/nybooks/presetation/books/BooksViewModel.kt
@@ -1,4 +1,49 @@
 package com.example.nybooks.presetation.books
 
-class BooksViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.nybooks.data.ApiService
+import com.example.nybooks.data.model.Book
+import com.example.nybooks.data.response.BookBodyResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class BooksViewModel : ViewModel() {
+
+    val booksLiveData: MutableLiveData<List<Book>> = MutableLiveData()
+
+    fun getBooks() {
+//        booksLiveData.value = getFakeRequestBooks()
+        ApiService.service.getBooks().enqueue(object : Callback<BookBodyResponse> {
+            override fun onResponse(call: Call<BookBodyResponse>, response: Response<BookBodyResponse>) {
+                if (response.isSuccessful) {
+                    val listBooks: MutableList<Book> = mutableListOf()
+                    response.body()?.let { bookBodyResponse ->
+                        for (result in bookBodyResponse.bookResultsResponse) {
+                            val book: Book = Book(
+                                title = result.bookDetailsResponse[0].title,
+                                author = result.bookDetailsResponse[0].author
+//                                description = result.bookDetailsResponse[0].description
+                            )
+                            listBooks.add(book)
+                        }
+                    }
+                    booksLiveData.value = listBooks
+                }
+            }
+            override fun onFailure(call: Call<BookBodyResponse>, t: Throwable) {
+                throw t.fillInStackTrace()
+            }
+        })
+    }
+
+    /*fun getFakeRequestBooks() : List<Book> {
+        return listOf<Book>(
+            Book("Titulo Um", "Autor A"),
+            Book("Titulo Dois", "Autor B"),
+            Book("Titulo Três", "Autor C")
+        )
+    }*/
+
 }
