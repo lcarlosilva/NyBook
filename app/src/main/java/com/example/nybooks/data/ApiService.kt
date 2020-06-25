ec7ae85dad73556f0e2591ab847b0ca9990ce9df
Luiz Carlos <luiz.silva@luizalabs.com>
2020-06-25 19:12:49 -0300

    correcao erro 'I/O error during system call, Connection reset by peer'

--- app/src/main/java/com/example/nybooks/data/ApiService.kt
+++ app/src/main/java/com/example/nybooks/data/ApiService.kt
@@ -1,6 +1,5 @@
 package com.example.nybooks.data
 
-import com.squareup.moshi.Moshi
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
@@ -13,7 +12,7 @@ object ApiService {
             .build()*/
 
         return Retrofit.Builder()
-            .baseUrl(" https://api.nytimes.com/svc/books/v3/")
+            .baseUrl("http://api.nytimes.com/svc/books/v3/")
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
